f97da1fe55313aa35c80960ecc2a3e8507c75d83
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-05-12 13:27:02 +0530

    Setup landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,9 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <a className={styles.email} href="mailto:manas@dining-philosophers.in">
+          Contact us
+        </a>
       </div>
     </header>
   );
@@ -37,7 +32,6 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main></main>
     </Layout>
   );
 }
